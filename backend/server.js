c83702c31503a62994d90366993750bba75b393a
IsSurajKadam <surajkadam1706004@gmail.com>
2025-04-16 20:49:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ import cloudinary from 'cloudinary'
 cloudinary.v2.config(
   {
   cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:process.env.CLOUDINARY_CLOUD_KEY,
-  api_secret:process.env.CLOUDINARY_CLOUD_KEY_SECRETE
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET
   }
 )
 app.listen(process.env.PORT,()=>
